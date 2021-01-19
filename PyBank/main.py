b4afc234b8dbe4519c0df6d3bbd0f2cea5facfd6
Jon Weber <jonweber0328@gmail.com>
2021-01-19 16:39:05 -0600

    added analysis header

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,16 +5,38 @@ import os
 import csv
 
 # Set file path to .csv
-csvpath = os.path.join('..','Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Calculate the total number of months included in the dataset.
+    # Define the functions
+def budget_data(by_month):
+    month = str(by_month[0])
+    profit_loss = int(by_month[1])
 
 # Open the .csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    print(csvreader)
+    # Make sure the file path is set up correctly(confirmed).
+    #csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
 
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+
+    # Print header of financial analysis
+    print("Financial Analysis")
+    print("--------------------------------")
+
+    
+
+        
+    print("Total Months: " + str(len(month)))
+    print(month)
+    
+
+
+
+
+
 
 
 
