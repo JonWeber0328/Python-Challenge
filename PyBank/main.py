186451ab4cb0fd2e8c21f9d07e310dc700411f54
Jon Weber <jonweber0328@gmail.com>
2021-01-19 15:17:49 -0600

    Added module imports

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@
 import os
 
 # Import module for reading CSV files
-import CSV
+import csv
 
 # Set file path to .csv
 csvpath = os.path.join('..','Resources', 'budget_data.csv')
@@ -12,6 +12,11 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+
 
 
 
