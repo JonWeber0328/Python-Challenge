d4cfd018c4f86b68a7050898ec32c2bc1fa85e32
Jon Weber <jonweber0328@gmail.com>
2021-01-20 13:49:58 -0600

    added total months calculation

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,31 +6,53 @@ import csv
 
 # Set file path to .csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
-
-# Calculate the total number of months included in the dataset.
-    # Define the functions
-def budget_data(by_month):
-    month = str(by_month[0])
-    profit_loss = int(by_month[1])
-
-# Open the .csv
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-
-    # Make sure the file path is set up correctly(confirmed).
+# Make sure the file path is set up correctly(confirmed).
     #csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
 
+# Open the .csv
+with open(csvpath) as csv_file:
 
-    # Print header of financial analysis
-    print("Financial Analysis")
-    print("--------------------------------")
+    # Split the data on commas
+    budget_data = csv.reader(csv_file, delimiter=",")
 
+
+    # Set header line
+    header = next(budget_data)
+    #print(f"{header}")
+
+    # Define the variables
+    net = 0
+    date = []
+    profit_loss = []
     
+    # Loop through the rows to calculate profit/loss
+    for row in budget_data:
+        #print(rows)
+
+        date.append(str(row[0]))
+        profit_loss.append(int(row[1]))
 
         
-    print("Total Months: " + str(len(month)))
-    print(month)
+        # Sum up net profit/loss
+        row_net = int(row[1])
+        net += row_net
+
+        
+        
+
+
+
+
+
+# Print header of financial analysis
+print("Financial Analysis")
+print("--------------------------------")
+print(f"Total Months: {net}")
+    
+
+
+     
     
 
 
