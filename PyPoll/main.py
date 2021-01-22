258a90f3d34dfe118fb1da33c9eb31eb105a3100
Jon Weber <jonweber0328@gmail.com>
2021-01-22 14:08:14 -0600

    Added complete list of candidates.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,18 @@
 # Import the os module to create file path.
 import os
 
-# Import module for reading CSV files
+# Import module for reading CSV files.
 import csv
+
+# Import module to get unique values in list.
+import numpy as np
 
 # Set file path to .csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 #print(csvpath)
 
 # Open the .csv
-with open(csvpath) as csvfile:
+with open(csvpath, 'r') as csvfile:
 
     # Split the data on commas
     election_data=csv.reader(csvfile, delimiter=",")
@@ -17,17 +20,37 @@ with open(csvpath) as csvfile:
 
     # Set header line
     header = next(election_data)
-    print(f"{header}")
+    #print(f"{header}")
     
     # Define the variables
     total_votes = 0
+    candidates = []
     
+    
+
     # Loop through the rows to find the total number of votes.
     for i in election_data:
 
         # Count the number of rows
         total_votes += 1
     #print(total_votes)
+
+        # get list of all candidate names
+        candidates.append(str(i[2]))
+    
+# Print a complete list of candidates who received votes
+# Function found on geeksforgeeks.org/python-get-unique-values-list/
+def unique(candidates):
+    x = np.array(candidates)
+    #print(np.unique(x))
+#print("The complete list of candidates who received votes:")
+unique(candidates)
+
+
+    
+
+
+
 
 # Print analysis 
 print("Election Results")
